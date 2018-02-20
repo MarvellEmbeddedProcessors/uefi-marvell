4fb15bb6c4b8ea88ab2a21dd77c8758cecbcdea6
Konstantin Porotchkin <kostap@marvell.com>
2018-02-20 11:13:52 +0200

    Terminal: allow multi-consumer protocol usage
    Use method EFI_OPEN_PROTOCOL_GET_PROTOCOL instead of
    EFI_OPEN_PROTOCOL_BY_DRIVER in order to allow different
    drivers to use the same protocol.
    
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Joe Zhou <shjzhou@marvell.com>

--- MdeModulePkg/Universal/Console/TerminalDxe/Terminal.c
+++ MdeModulePkg/Universal/Console/TerminalDxe/Terminal.c
@@ -485,7 +485,7 @@ TerminalDriverBindingStart (
                   (VOID **) &ParentDevicePath,
                   This->DriverBindingHandle,
                   Controller,
-                  EFI_OPEN_PROTOCOL_BY_DRIVER
+                  EFI_OPEN_PROTOCOL_GET_PROTOCOL
                   );
   ASSERT ((Status == EFI_SUCCESS) || (Status == EFI_ALREADY_STARTED));
 
